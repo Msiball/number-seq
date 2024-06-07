adb63475183958e69a6e5dd33eea9dba1bac8ef5
Manpreet Singh <manpreet0361.ms@gmail.com>
2024-06-06 23:26:19 -0600

    Add files via upload

---
+++ number sequence.js
@@ -0,0 +1,16 @@
+var output = [];
+var a=1;
+
+function fizzBuzz() {
+
+    if (a % 3 ==== 0){
+        output.push("Fizz");
+    }
+    else {
+         output.push(a);
+    }
+    a=a+1;
+    console.log(output);    
+}
+clear()
+
